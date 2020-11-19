f199f8a73c8ead3136ee9ef6000694546657b249
Daly KONTE <daly.konte@gmail.com>
2020-11-19 12:19:14 +0100

    ajout nav + footer

--- index.php
+++ index.php
@@ -9,26 +9,50 @@
     <title>Mon projet GIT</title>
 </head>
 <body>
-<div class="container">
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Mon projet GIT</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExample04">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+        </ul>
+
+    </div>
+</nav>
+
+<div class="container" style="min-height: 600px">
 
     <h1 class="alert alert-success display-4 text-center my-5 shadow-lg">Mon projet GIT</h1>
 
     <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-    <div class="carousel-item active">
-        <img src="image/slider1.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="image/slider1.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="image/slider2.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="image/slider3.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
     </div>
-    <div class="carousel-item">
-        <img src="image/slider2.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-        <img src="image/slider3.jpg" class="d-block w-100" alt="...">
-    </div>
-    </div>
+
 </div>
 
+<footer class="bg-dark text-center text-white p-3">
+    &copy; 2020 - Daly KONTE - Mon projet GIT
+</footer>
 
-</div>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
